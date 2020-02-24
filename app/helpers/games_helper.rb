abed1a564eb0572c8f165bceddd3565ec33fc6b9
Subject81 <Subject8181@gmail.com>
2020-02-24 12:24:48 -0600

    Initial commit

---
+++ app/helpers/games_helper.rb
@@ -0,0 +1,13 @@
+module GamesHelper
+    def add_timestamp_to_msg(message)
+        time = Time.now
+        full_message = message + " (" + time.strftime("%A %B %d, %Y") + time.strftime("%I:%M:%S %P") + ")</span><br>";
+        session[:activities].insert(0, full_message )
+
+        num_activites = session[:activities].length
+        if num_activites == 10
+            session[:activities].delete_at(num_activites - 1)
+        end
+
+    end 
+end
